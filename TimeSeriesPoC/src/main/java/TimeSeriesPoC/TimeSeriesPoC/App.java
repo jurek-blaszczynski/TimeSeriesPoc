37a5aefc76d55dbdab40f927263511cea78c8f64
Wojciech Mioduszewski <wielkimiodas@gmail.com>
2016-06-23 01:32:08 +0200

    working beta proof of concept

--- TimeSeriesPoC/src/main/java/TimeSeriesPoC/TimeSeriesPoC/App.java
+++ TimeSeriesPoC/src/main/java/TimeSeriesPoC/TimeSeriesPoC/App.java
@@ -1,20 +1,13 @@
 package TimeSeriesPoC.TimeSeriesPoC;
 
-import pl.poznan.put.TimeSeries.Classifying.BasicNgramClassifier;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
-        
-        BasicNgramClassifier bc = new BasicNgramClassifier();
-        System.out.println(bc.hashCode());
-        
+        try {
+			PoC.exportArff();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
--- TimeSeriesPoC/src/main/java/TimeSeriesPoC/TimeSeriesPoC/PoC.java
+++ TimeSeriesPoC/src/main/java/TimeSeriesPoC/TimeSeriesPoC/PoC.java
@@ -1,13 +1,21 @@
 package TimeSeriesPoC.TimeSeriesPoC;
 
+import pl.poznan.put.TimeSeries.Classifying.Datasets;
+import pl.poznan.put.TimeSeries.Constants.DivisionOptions;
+import pl.poznan.put.TimeSeries.Workflows.DominantWorkflow;
+import pl.poznan.put.TimeSeries.Workflows.WorkflowBase;
+
 public class PoC {
-
-	public PoC() {
-		super();
-		// TODO Auto-generated constructor stub
+	
+	public static void exportArff() throws Exception{
+		System.out.println("Arff export has started.");
+		
+		Datasets dataset = Datasets.PATIENTS;
+		boolean isGlaucoma = dataset == Datasets.PATIENTS;
+		
+		WorkflowBase workflow = new DominantWorkflow(DivisionOptions.Regular, isGlaucoma);
+		
+		String path = workflow.saveArff(null, "Exported arff file.arff");
+		System.out.println("Arff for " + dataset.name() + " has been exported to " + path);
 	}
-	
-	 
-	
-
 }
